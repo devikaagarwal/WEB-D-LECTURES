88629bdba358d8dad7f698aeb2cab795e34806a6
devikaagarwal <121999426+devikaagarwal@users.noreply.github.com>
2024-10-22 08:51:36 +0530

    Lec-18

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
-let arr=[2,3,4,5,6];
-// let arr1=arr;
-let arr1=[7,8,9,10,11];
-let arr2=[...arr,...arr1];//spread operator,rest,arrow
-arr1.push("hello")
-// let arr2=arr.slice(0);
-let arr3=arr.concat(arr1);
-console.log(arr);
-// console.log(arr1);
-// console.log(arr2);
-console.log(arr3);
+//function statement
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(4,6));
 
-let person={
-    fullname:"abcde",
-    age:23,
-    salary:343534,
-    id:233,
-    projectdetail:{
-        projectId:123,
-        projectTitle:"Code js"
-    }
+const multiply = function(a,b){
+    return a*b;
 }
-console.log(person);
-console.log(person.age);//object chaining
-console.log(person["age"]);
-for(let key in person){
-    console.log(key);
-}
+console.log(multiply(4,6));
+// function expression
 
-for(let key in person){
-    console.log(person[key]);
+// const sum = function(){
+
+// }
+// arrow function
+const sub = (a,b) => {
+    return a-b;
 }
-for(let key in person){
-    console.log(person.key);
-}
+console.log(sub(15,3));
+// const sum = () => {
+
+// }
