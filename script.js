cd7bdc63d4e72cac5f4cd890968d16885a03cc4d
devikaagarwal <121999426+devikaagarwal@users.noreply.github.com>
2024-10-22 08:58:42 +0530

    Lec-22

--- script.js
+++ script.js
@@ -1,53 +1,24 @@
-console.log("hello");
-// function waitSec(){
-//     let curr_time=new Date().getTime();
-//     let fut_time=curr_time+1000;
-//     while(new Date().getTime()<fut_time);
-// }
-// function waitNSec(n){
-//     for(let i=0;i<n;i++){
-//         waitSec();
-//     }
-// }
-// waitNSec(6);
-//-----------------------------------------------------------------------------//
-setTimeout(()=>{
-    // console.log("inside setTimeout");
-    
-},5000)
-//------------------------------------------------------------------------------//
-let id=setInterval(() => {
-    // console.log("data is fetched");
-    
-}, 1000);
-
-// setInterval(()=>{
-//     clearInterval(id);
-// },8000)
-// console.log("world");
-// ----------------------------------------------------------------------------//
-let student=(function IIFE(){
-    let marks=90;
-    function student(){
-        details();
-    }
-    function details() {
-        console.log(`percentage: ${(marks/100)*100}`);
-    }
-    return student;
-})()
-student();
-
-
-// ((a,b)=>{
-//     console.log(`SUM OF a & b ${a+b}`);
-// })(10,15)
-
-function placeOrder(){
+function downloadBook(unzipped){
     setTimeout(()=>{
-        console.log("order placed");
-        fg();
-    }
+        console.log("book downloaded");
+        unzipped();
+    },5000)
+}
+//callback inside a callback is called pyramid of doom
+function unzipped(){
+    setTimeout(()=>{
+        console.log("book unzipped");
+        setTimeout(()=>{
+            console.log("book parsed");
+            setTimeout(()=>{
+                console.log("book read and analyzed properly");
+                setTimeout(()=>{
+                    console.log("book uploaded successfully");
+                },3000)
+            },2000)
+        },3000)
+    },2000)
 }
 
 
+downloadBook(unzipped);
